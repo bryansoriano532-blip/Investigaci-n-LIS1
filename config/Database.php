3e1c803e6695c748bca5a0ca8e3451c8cbf6d73e
bryansoriano532-blip <bryansoriano532@gmail.com>
2026-02-24 21:36:34 -0600

    Add files via upload

--- config/Database.php
+++ config/Database.php
@@ -1,1 +1,20 @@
+<?php
+class Database {
+    private $host = "localhost";
+    private $db_name = "tienda_libros";
+    private $username = "root";
+    private $password = "";
+    public $conn;
 
+    public function getConnection() {
+        $this->conn = null;
+        try {
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch(PDOException $e) {
+            echo "Error de conexión: " . $e->getMessage();
+        }
+        return $this->conn;
+    }
+}
+?>
